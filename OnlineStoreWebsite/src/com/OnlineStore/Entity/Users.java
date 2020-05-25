7ad38856d447a1619ddb35860c6775d2b0bc9528
mambalde <mambalde@naver.com>
2020-05-25 17:28:40 +0900

    core packages and project layout

---
+++ OnlineStoreWebsite/src/com/OnlineStore/Entity/Users.java
@@ -0,0 +1,52 @@
+package com.OnlineStore.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Users {
+	private Integer userId;
+	private String email;
+	private String fullname;
+	private String password;
+
+	@Column(name = "user_id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Column(name = "full_name")
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
